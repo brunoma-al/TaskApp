2bae012e760240bc3c7e110cecaa67fec4df22c9
Bruno <brugacag@gmail.com>
2024-04-04 19:25:24 -0300

    Novas telas criadas e configuradas.

--- src/main/java/com/bruno/android/taskapp/ui/HomeFragment.kt
+++ src/main/java/com/bruno/android/taskapp/ui/HomeFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import com.bruno.android.taskapp.R
 import com.bruno.android.taskapp.databinding.FragmentHomeBinding
 import com.bruno.android.taskapp.databinding.FragmentLoginBinding
+import com.bruno.android.taskapp.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
@@ -20,6 +22,28 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabs()
+    }
+
+    private fun initTabs(){
+
+        val pagerAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager2.adapter = pagerAdapter
+
+        pagerAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pagerAdapter.addFragment(DoingFragment(), R.string.status_task_doing)
+        pagerAdapter.addFragment(DoneFragment(), R.string.status_task_done)
+
+        binding.viewPager2.offscreenPageLimit = pagerAdapter.itemCount
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2){tab, position ->
+            tab.text = getString(pagerAdapter.getTitle(position))
+        }.attach()
     }
 
     override fun onDestroyView() {
