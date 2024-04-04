9fd2018475f8863a574f9ed6d0c72f5e97d00e51
Bruno <brugacag@gmail.com>
2024-04-04 18:07:55 -0300

    Initial commit.

---
+++ src/main/java/com/bruno/android/taskapp/ui/auth/RecoverAccountFragment.kt
@@ -0,0 +1,23 @@
+package com.bruno.android.taskapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bruno.android.taskapp.R
+import com.bruno.android.taskapp.databinding.FragmentLoginBinding
+
+class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
